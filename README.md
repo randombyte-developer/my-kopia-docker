cfe872f33307950edc423be3a62d44a3fba35b30
Sven Rahn <apps.randombyte@gmail.com>
2024-03-31 13:37:54 +0200

    Add daily timer

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # my-kopia-docker
+## kopia image
+Runs one instance of kopia. It always connects to a local network share as its source and supports SMB (CIFS driver) and B2 targets.
+
+## scheduler image
+Runs a given command each day for all specified containers with a delay inbetween.
--- scheduler/run.sh
+++ scheduler/run.sh
@@ -5,6 +5,8 @@
 container_names=(${CONTAINER_NAMES//;/ })
 
 while true; do
+    # What until 00:10 each day and then sequentially execute the command with the specified delay inbetween each container
+    sleep $((($(date -f - +%s- <<<00:10$' tomorrow\nnow')0)%86400))
     for container_name in "${container_names[@]}"; do
         # Check if the container exists (might be down due to maintenance)
         if container_id=$(docker ps --filter "name=$container_name" --format "{{.ID}}"); then
